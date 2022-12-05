e10b1852903621c07f2ccd5450a9812c6c9bb29f
vdnfgg <gladius2905@gmail.com>
2022-12-05 18:10:03 +0100

    Update wstawianie-pracaDomowa.py

---
+++ wstawianie-pracaDomowa.py
@@ -0,0 +1,24 @@
+"""
+Praca domowa
+Podaj specyfikację zadania i napisz program realizujący wstawianie liczby wprowadzonej z
+klawiatury lub podanej jako parametr funkcji do uporządkowanego ciągu liczbowego w takie miejsce,
+aby po jej wstawieniu ciąg nadal był uporządkowany. Algorytm powinien wyznaczać pozycję
+wstawienia liczby do uporządkowanego zbioru. Do wstawiania możesz wykorzystać metodę insert().
+"""
+
+def sortadd(array, a):
+
+    # szukanie miejsca do wstawienia
+    for i in range(len(array)):
+        if array[i] > a:
+            index = i
+            break
+
+    # wstawianie do tablicy
+    if index == len(array):
+        array = array[:index] + [a]
+    else:
+        array = array[:index] + [a] + array[index:]
+    return array
+
+print(sortadd([1,2,3,5,6], 4))
