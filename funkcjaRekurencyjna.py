8f8bd7a5cd7f0b0d4242e694b2a13a808969e261
vdnfgg <119745907+vdnfgg@users.noreply.github.com>
2023-01-02 16:04:52 +0100

    Add files via upload

--- funkcjaRekurencyjna.py
+++ funkcjaRekurencyjna.py
@@ -14,16 +14,9 @@ def silniaIteracja(n):
 
 print(silniaIteracja(5))
 
+
 # zad 2
 def newton(n, k):
-    n2 = 1
-    for i in range(2, n + 1):
-        n2 *= i
-
-    k2 = 1
-    for i in range(2, k + 1):
-        k2 *= i
-
-        return n2 / k2
+    return silniaIteracja(n) / (silniaIteracja(k) * silniaIteracja(n - k))
 
 print(newton(4, 3))
